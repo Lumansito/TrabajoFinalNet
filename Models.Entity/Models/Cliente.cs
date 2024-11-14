1a779f210984ee757f82e7c6442c6503db86e830
Nicolás Escobar <nicoescobar666@gmail.com>
2024-11-14 00:03:38 -0300

    Reapply "Merge branch 'main' of https://github.com/Lumansito/TrabajoFinalNet"
    This reverts commit dc555b7356cea346a8dd64920d706e5fe0567302.

--- Models.Entity/Models/Cliente.cs
+++ Models.Entity/Models/Cliente.cs
@@ -17,8 +17,6 @@ namespace Models.Entity.Models
 
         public string Telefono { get; set; } = string.Empty;
 
-        public bool Activo { get; set; }
-
         public DateTime FechaAlta { get; set; }
 
         public DateTime FechaNacimiento { get; set; }
